e54aebac5138ee17863ca3326f2db74c57bd6dfb
Nick Ramsay <NickRamsay1989@gmail.com>
2020-05-26 20:56:44 +1000

    Yarn uninstalled, recommitting to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
