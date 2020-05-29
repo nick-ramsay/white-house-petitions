4534e5d34b133ace6f469dfeb5b612925d12ace9
Nick Ramsay <NickRamsay1989@gmail.com>
2020-05-29 15:43:18 +1000

    Adding express server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+//const mongoose = require("mongoose");
+//const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Setting headers for CORS Policies
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
